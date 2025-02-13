2efcaea60c23d406c8aea43b7a1b1e8a709d08ab
Arya Gopikrishnan <aryagxr@gmail.com>
2025-02-13 14:13:17 -0500

    shuffled layer norm

--- layernorm/main.cu
+++ layernorm/main.cu
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include <cuda.h>
 
-#include "kernels/smem-layernorm.cu"
+#include "kernels/shfl-layernorm.cu"
 
 int main(){
 
-    int M = 1024;
-    int N = 1024;
+    int M = 3;
+    int N = 3;
 
     size_t matrix_size = M*N*sizeof(float);
     float *X_input, *P_output;
@@ -26,21 +26,21 @@ int main(){
 
     cudaMemcpy(D_input, X_input, matrix_size, cudaMemcpyHostToDevice);
 
-    run_smem_ln(D_input, D_output, M, N);
+    run_shfl_ln(D_input, D_output, M, N);
 
     cudaMemcpy(P_output, D_output, matrix_size, cudaMemcpyDeviceToHost);
 
     printf("Input matrix: \n");
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
+    for(int i=0; i<M; i++){
+        for(int j=0; j<N; j++){
             printf("%f", X_input[i*N+j]);
         }
         printf("\n");
     }
 
     printf("Output matrix: \n");
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
+    for(int i=0; i<M; i++){
+        for(int j=0; j<N; j++){
             printf("%f", P_output[i*N+j]);
         }
         printf("\n");
